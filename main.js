ca8d1ddef479f07a5d58ca7031186d1a6ab53364
TechyDog <flowdogshop@gmail.com>
2018-03-08 19:58:26 -0500

    added formspree for sending emails

--- main.js
+++ main.js
@@ -10,3 +10,21 @@ function scrollAnimationWhenNavbarIsClicked() {
         }, 1000)
     });
 }
+
+function test() {
+    $.ajax({
+        url: "https://formspree.io/flowdogshop@gmail.com",
+        headers: { "Accept": "application/json;" },
+        method: "POST",
+        data: {
+            message: "hello!"
+        },
+        dataType: "json",
+        success: function (data) {
+            console.error(data);
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    })
+}
