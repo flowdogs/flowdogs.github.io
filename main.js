b24ec8e128922391371556c9df4e1e15db1b1ecf
TechyDog <flowdogshop@gmail.com>
2018-03-08 20:51:09 -0500

    jquery confirm test

--- main.js
+++ main.js
@@ -11,20 +11,37 @@ function scrollAnimationWhenNavbarIsClicked() {
     });
 }
 
-function test() {
-    $.ajax({
-        url: "https://formspree.io/flowdogshop@gmail.com",
-        headers: { "Accept": "application/json;" },
-        method: "POST",
-        data: {
-            message: "hello!"
-        },
-        dataType: "json",
-        success: function (data) {
-            console.error(data);
-        },
-        error: function (error) {
-            console.error(error);
-        }
-    })
+function orderNow() {
+    $.dialog({
+        title: 'JakeChipman pro model',
+        content: 'url:dialogcontent.html',
+            // '<div class="container">' +
+            //     '<form action="https://formspree.io/flowdogshop@gmail.com" method="POST">' +
+            //         '<div class="row">' +
+            //             '<div class="col-lg-4 col-md-4 col-sm-12">' +
+            //                 '<h1>Get in Touch</h1>' +
+            //             '</div>' +
+            //             '<div class="col-lg-8 col-md-8 col-sm-12 right">' +
+            //                 '<div class="form-group">' +
+            //                     '<input type="text" class="form-control form-control-lg" placeholder="Your Name" name="name">' +
+            //                 '</div>' +
+            //                 '<div class="form-group">' +
+            //                     '<input type="email" class="form-control form-control-lg" placeholder="YourEmail@email.com" name="_replyto">' +
+            //                 '</div>' +
+            //                 '<div class="form-group">' +
+            //                     '<textarea class="form-control form-control-lg"></textarea>' +
+            //                 '</div>'+
+            //                 '<input type="submit" class="btn btn-secondary btn-block" value="Send Order" name="">' +
+            //             '</div>' +
+            //         '</div>' +
+            //     '</form>' +
+            // '</div>',
+        closeIcon: true,
+        columnClass: 'col-md-6 col-md-offset-6 col-xs-6 col-xs-offset-6',
+        containerFluid: true,
+        draggable: true,
+        theme: 'bootstrap',
+        backgroundDismiss: true,
+        escapeKey: true,
+    });
 }
